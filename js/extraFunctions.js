3dd215592bed51550cfe8fec5cc5cf188ed0f06d
florisweb <floris@florisweb.dev>
2023-05-20 22:09:02 +0200

    Added a little pointer that indicates your current location, plus automatically zoom to that location on load.

--- js/extraFunctions.js
+++ js/extraFunctions.js
@@ -8,3 +8,15 @@ function setTextToElement(element, text) {
   a.text = text;
   element.append(a);
 }
+
+
+
+function getLocation() {
+  return new Promise((resolve, error) => {
+    if (!navigator.geolocation) {
+      error("Geolocation is not supported by your browser");
+    } else {
+      navigator.geolocation.getCurrentPosition((_result) => resolve(_result.coords), error);
+    }
+  })
+}
--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@
 
 // Options for map
 var options = {
-  lat: 52.3,
-  lng: 5,
+  lat: -52.3,
+  lng: -5,
   zoom: 8,
   style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 }
@@ -17,18 +17,29 @@ var mappa = new Mappa('Leaflet');
 var myMap;
 
 var canvas;
+let ctx;
+
+let curLocation;
 
 async function setup() {
   await DataManager.setup();
   canvas = createCanvas(document.body.offsetWidth, document.body.offsetHeight);
+  ctx = canvas.drawingContext;
+  ctx.circle = function(_x, _z, _radius) {
+    ctx.beginPath();
+    ctx.arc(_x, _z, _radius, 0, 2 * Math.PI);
+  }
 
-  // Create a tile map and overlay the canvas on top.
+  curLocation = await getLocation().catch(alert);
+  if (curLocation)
+  {
+    options.lat = curLocation.latitude;
+    options.lng = curLocation.longitude;
+    options.zoom = 12;
+  }
+
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
-
-  // Load the data
-
-  // Only redraw the meteorites when the map change and not every frame.
   myMap.onChange(onChange);
 
   fill('rgba(255, 0, 0, .1)');
@@ -41,6 +52,7 @@ function onChange() {
   clear();
   drawTiles();
   drawPoints();
+  drawCurLocation();
 }	
 
 
@@ -55,6 +67,7 @@ function drawTiles() {
 
     let opacity = (1 - Math.pow(2, -tile.counts / 10)) / 2;
     fill('rgba(255, 0, 0, ' + opacity + ')');
+    stroke('#f00');
     rect(pos.x, pos.y, dx, dy);
   }
 }
@@ -67,9 +80,69 @@ function drawPoints() {
     if (!myMap.map.getBounds().contains({lat: point.lat, lng: point.long})) continue;
     let pos = myMap.latLngToPixel(point.lat, point.long);
     fill('#f00');
+    stroke('#f00');
     ellipse(pos.x, pos.y, 5, 5);
   }
 }
+
+function drawCurLocation() {
+  if (!curLocation) return;
+  if (!myMap.map.getBounds().contains({lat: curLocation.latitude, lng: curLocation.longitude})) return;
+  let pos = myMap.latLngToPixel(curLocation.latitude, curLocation.longitude);
+
+  drawPointToCanvas(pos.x, pos.y, 10, "#f00")
+
+  function drawPointToCanvas(x, z, radius, colour) {
+    let r = 10;
+
+    if (radius) drawRadius(x, z, radius, colour);
+    drawNeedle(x, z, r);
+
+    let gradient = ctx.createLinearGradient(x - r, z - r, x + r, z + r);
+    gradient.addColorStop(0, colour);
+    gradient.addColorStop(1, "#030303");
+    
+    ctx.fillStyle = gradient;
+    ctx.circle(x, z - 2 * r, r);
+    ctx.fill();
+  }
+
+  function drawNeedle(x, z, r) {
+    ctx.fillStyle = "#fff";
+    ctx.beginPath();
+    ctx.moveTo(x, z);
+    ctx.lineTo(x - r, z - 2 * r - 1);
+    ctx.lineTo(x, z - 2 * r - 1);
+    ctx.fill();
+
+    let grd = ctx.createLinearGradient(x, z - 2 * r - 1, x + 0.5 * r, z);
+    grd.addColorStop(0, "#fff")
+    grd.addColorStop(1, "#aaa");
+    ctx.fillStyle = grd;
+    
+    ctx.beginPath();
+    ctx.moveTo(x, z);
+    ctx.lineTo(x + r, z - 2 * r - 1);
+    ctx.lineTo(x, z - 2 * r - 1);
+    ctx.fill();
+  }
+
+  function drawRadius(_x, _z, _radius, _colour) {
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = _colour;
+    ctx.fillStyle = _colour;
+    ctx.globalAlpha = 0.2;
+
+    ctx.circle(_x, _z, _radius);
+    ctx.closePath();
+    ctx.fill();
+    ctx.globalAlpha = 1;
+    ctx.stroke();
+  }
+}
+
+
 
 
 function calcMetresPerPixel() {
